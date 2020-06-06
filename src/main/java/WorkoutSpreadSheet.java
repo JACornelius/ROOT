d73236c1deaad93d15b8eaca57fc3a364ddd1f09
Josephine Cornelius <jos.a.cornelius@gmail.com>
2020-06-05 22:22:49 -0500

    basic reading from google's api given sheet

---
+++ src/main/java/WorkoutSpreadSheet.java
@@ -0,0 +1,14 @@
+public class WorkoutSpreadSheet {
+    private String spreadSheetId;
+
+    public String getSpreadSheetId() {
+        return spreadSheetId;
+    }
+
+    public void setSpreadSheetId(String spreadSheetId) {
+        this.spreadSheetId = spreadSheetId;
+    }
+
+    // function to create new spread sheet
+    // function to get values of spread sheet with id, return list of values object
+}
